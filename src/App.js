94dbc4fdb9876f8b945aa1d02a4c246bacc99738
Summer Burgess <77293256+BlushingSaturn@users.noreply.github.com>
2022-06-22 19:26:27 -0400

    add image slideshow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
 
+import placeholder from './sliderPhotos/placeholder.jpg'
+import placeholder2 from './sliderPhotos/placeholder2.jpg'
+import placeholder3 from './sliderPhotos/placeholder3.jpg'
+import placeholder4 from './sliderPhotos/placeholder4.jpeg'
 
 import NavBar from './components/NavBar';
 import Button from './components/Button';
+import Slider from './components/Slider';
+
+const ImageData = [
+  {
+    image:
+      placeholder,
+  },
+  {
+    image:
+      placeholder2,
+  },
+  {
+    image:
+      placeholder3,
+  },
+  {
+    image:
+      placeholder4,
+  },
+];
 
 function App() {
   return (
@@ -10,12 +34,15 @@ function App() {
       <NavBar activeHome/>
       <div className="landing-screen">
         <div className='info-container'>
-          <h1>Summer Burgess</h1>
+          <h1>Summer</h1><h1 className="last-name">Burgess</h1>
           <h2>Web Designer and Game Developer</h2>
           <div className="button-container">
             <Button border link='https://google.com' label='Portfolio'/>
             <Button link='https://google.com' label='Contact Me'/>
           </div>
+        </div>
+        <div className='carousel-container'>
+          <Slider slides={ImageData} autoPlay={2}/>
         </div>
       </div>
     </div>
---
+++ src/components/SlideImage.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+
+export const SlideImage = styled.img`
+    border: .3rem solid white;
+    border-radius: .5rem;
+    box-shadow: #000 0 0.2rem 1rem;
+    height: 35rem;
+    margin: 0 1rem;
+    object-fit: cover;
+    width: 35rem;
+`;
+
+export const StyledSlider = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+`;
